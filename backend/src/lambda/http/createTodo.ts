20edb8c6601326237a8a343ac63c065da619032e
EmmanuelStan12 <emmanuelstanley753@gmail.com>
2022-08-02 22:16:04 +0100

    Submission

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -5,6 +5,17 @@ import { createTodo } from '../../businessLogic/todos'
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
+
+    if (newTodo.dueDate.length < 1 || newTodo.name.length < 3) {
+      return {
+        statusCode: 400,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true
+        },
+        body: "Invalid Schema, Please fill in the neccessary fields, Empty fields and names shorter than 3 are not allowed"
+      }
+    }
     const newItem = await createTodo(newTodo, event);
     return {
       statusCode: 201,
